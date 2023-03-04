1a624a9dc1189e538834e4a93d5c58260826fe4d
Тимофеев Кирилл 153505 <kirkat6@gmail.com>
2023-03-04 14:10:26 +0300

    Ready to debug main logic

--- logic.py
+++ logic.py
@@ -7,6 +7,9 @@ ops = {}
 
 def analyze(text: str):
     check_func(text)
+    check_vars(text)
+    check_ops(text)
+    return {"operands": vars, "operators": ops}
 
 
 def check_func(text: str):
@@ -17,21 +20,31 @@ def check_func(text: str):
 
 def check_vars(text: str):
     lst = re.findall(variable, text)
+    lst = set(lst)
     for var in lst:
         vars[var[0]] = len(re.findall('\b'+var[0]+'\b', text))
 
     lst = re.findall(string_literal, text)
+    lst = set(lst)
     for lit in lst:
         vars[lit] = len(re.findall(lit, text))
 
     lst = re.findall(char_literal, text)
+    lst = set(lst)
     for chr in lst:
         vars[chr] = len(re.findall(chr, text))
 
     lst = re.findall(numb_literal, text)
+    lst = set(lst)
     for numb in lst:
         vars[numb] = len(re.findall('\b'+numb+'\b', text))
 
+
+def check_ops(text: str):
+    lst = re.findall(operators, text)
+    lst = set(lst)
+    for op in lst:
+        ops[op] = len(re.findall('\b'+op+'\b', text))
 
 # find funcs
 # add to operators
--- regexes.py
+++ regexes.py
@@ -1,10 +1,11 @@
 types: str = r"(?:int|char|float|double|bool|long|long long|short|size_t|void|long double|string|unsigned long long|void|unsigned char|signed char)"
 
-variable: str = r'(?:const)?' + types + r'\s*(\w+)\s*(=\s*[^;])?'
-func: str = types + r'(\w+)\s*\((?:' + variable + ')*\)'
+variable: str = r'(?:const)?' + types + r'\s*\**\s*(\w+)\s*(=\s*[^;])?'
+func: str = types + r'\s+(\w+)\s*\((?:' + variable + ',?)*\)'
 
-operators: str = r'(\+|\=|\+=|-|-=|--|\+\+|/|/=|\*|\*=|(?:std::\s*)?cout(?:\s*<<\w+)+|(?:std::\s*)?cin(?:\s*>>\w+)+|\<|\>|!=|==|;|&&|\|\||&|\||\^|for|while|>=|<=)'
+operators: str = r'(\+|\=|\+=|-|-=|--|\+\+|/|/=|\*|\*=|(?:std::\s*)?cout(?:\s*<<\w+)+|(?:std::\s*)?cin(?:\s*>>\w+)+|\<|\>|!=|==|;|&&|\|\||&|\||\^|for|while|>=|<=|if|\(|\))'
 
-for_expr: str = ''
-while_expr: str = ''
-if_expr: str = ''
+
+string_literal: str = r'"(?:[^"]|(?<=\\)")*"'
+numb_literal: str = r'\d+(?:\.\d+)?'
+char_literal: str = r"'..?'"
